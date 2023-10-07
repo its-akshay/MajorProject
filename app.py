f59e3fb540d0c88b924a4680b6e2fd7c6862b485
Akshay Keswani <71098450+its-akshay@users.noreply.github.com>
2023-10-07 21:54:13 +0530

    Image-and-Routing-Changes

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ def predict():
 
     if prediction[0] in crop_dict:
         crop = crop_dict[prediction[0]]
-        result = "{} is the best crop to be cultivated right there".format(crop)
+        # result = "{} is the best crop to be cultivated right there".format(crop)
+        result = "{}".format(crop)
+
     else:
         result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
 
